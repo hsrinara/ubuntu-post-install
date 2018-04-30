4d49115dccdc8d91b588377f12e9921f7b5dc33d
Harsha Sri-Narayana <harshavsn@gmail.com>
2018-04-30 10:20:59 +0100

    initial commit

---
+++ postinstall.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo apt-get -y install vim
+sudo apt-get -y install firefox
+sudo apt-get -y install gedit
+sudo apt-get -y install gradle
+sudo apt-get -y install keepassx
+sudo apt-get -y install python 
+sudo apt-get -y install pip
+sudo apt-get -y install terminator
+sudo apt-get -y install vlc
+sudo apt-get -y install wireshark
+sudo apt-get -y install atom
+sudo apt-get -y install eclipse
+
+cp .gitconfig ~/
+mkdir ~/applications
+mkdir ~/code
+mkdir ~/applications/scripts
+echo "export PATH=$PATH:~/applications/scripts"
+echo "pulseaudio -k && sudo alsa force-reload" >> ~/applications/scripts/restart_sound.sh
+chmod u+x ~/applications/scripts/restart_sound.sh
+
+echo "export PS1='\\w:\$(git branch 2>/dev/null | grep '^*' | colrm 1 2)\$ '" >> ~/.bash_profile
+
