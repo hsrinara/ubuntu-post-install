421896581af881bff7d3552226c4cc564236d205
Harsha Sri-Narayana <hsrinara@users.noreply.github.com>
2018-06-05 16:34:53 +0100

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -2,6 +2,7 @@
 
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['']"
+gsettings set org.gnome.desktop.wm.keybindings begin-move "['']"
 
 sudo apt-get -y install vim
 sudo apt-get -y install firefox
