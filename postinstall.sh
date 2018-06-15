fd7069ea2ce269d52015893c8037db5eadd1bae4
Harsha Sri-Narayana <hsrinara@users.noreply.github.com>
2018-06-15 09:49:35 +0100

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -40,12 +40,12 @@ mkdir -p ~/.config/terminator
 cp ./terminator-config ~/.config/terminator
 
 #echo "export PS1='\\w:\$(git branch 2>/dev/null | grep '^*' | colrm 1 2)\$ '" >> ~/.bash_profile
-echo > ~/.bash_profile << EOL
+#TODO should echo into bash_profile
 parse_git_branch() {
     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
 export PS1="\w:\[\033[32m\]\$(parse_git_branch)\[\033[00m\] $ "
-EOL
+
 
 
 # has dialog prompt :'(
